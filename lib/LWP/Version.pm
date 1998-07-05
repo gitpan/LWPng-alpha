8ab243621d03652237b053a72358ee1e80bd5d71
Gisle Aas <gisle@ActiveState.com>
1998-07-05 14:49:24 +0000

    Import of GAAS/LWPng-alpha-0.22 from CPAN.
    gitpan-cpan-distribution: LWPng-alpha
    gitpan-cpan-version:      0.22
    gitpan-cpan-path:         GAAS/LWPng-alpha-0.22.tar.gz
    gitpan-cpan-author:       GAAS
    gitpan-cpan-maturity:     released

--- lib/LWP/Version.pm
+++ lib/LWP/Version.pm
@@ -1,6 +1,6 @@
 package LWP::Version;
 
-$VERSION       = '0.21';  # $Date: 1998/06/27 09:29:16 $
+$VERSION       = '0.22';  # $Date: 1998/07/05 14:36:12 $
 $PRODUCT_TOKEN = "libwww-perl/ng-alpha-$VERSION ($^O)";
 
 1;
